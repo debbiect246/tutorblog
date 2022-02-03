b2b05719786460db28eac32827e2c383a4f8fc38
Deborah Thompson <deb246@btinternet.com>
2022-02-03 14:32:36 +0000

    created views and urls for blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('<h1>Blog Home</h1>')
+    #this function links to urls.py in blog directory so view knows where to go for url
+
+def about(request):
+    return HttpResponse('<h1>Blog About</h1>')
--- tutorblog/urls.py
+++ tutorblog/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),  #finds the urls for the blog home page in the blog app
 ]
